83332f5dc9882a63919a182ae8df3a3f9c9f5163
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2020-01-24 21:34:39 +0100

    Split webpack config to dev / prod

---
+++ webpack.config.base.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const { h64 } = require('xxhashjs')
+const webpack = require('webpack')
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const root = path.resolve(__dirname)
+
+const getLocalIdent = (context, localIdentName, localName) => {
+  const generatedClassname =
+    context.resource
+      .split(path.join(root, 'src/components'))[1]
+      .substr(1)
+      .split('.scss')[0]
+      .replace(/\//g, '-') +
+    '-' +
+    localName
+  return generatedClassname + '_' + h64(generatedClassname, 0xabcd).toString(36)
+}
+
+module.exports = (attrs = {}, options = {}) => {
+  const mode = attrs.mode || 'production'
+  const extraLoader = options.extraLoader ? [{ loader: options.extraLoader }] : []
+  return {
+    mode,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index_bundle.js',
+    },
+    module: {
+      rules: [
+        { test: /\.(js|ts)x?$/, exclude: /node_modules/, use: ['babel-loader', ...extraLoader] },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: { getLocalIdent },
+                importLoaders: 3,
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: require('sass'),
+                sassOptions: {
+                  fiber: false,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(svg|eot|otf|ttf|woff2?)$/,
+          loader: 'file-loader',
+        },
+      ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      host: 'client.lvh.me',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    ...attrs,
+  }
+}
