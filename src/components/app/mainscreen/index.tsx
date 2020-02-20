846bbdca3f6e974386abb34a0590087efa0a5927
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2020-02-20 20:21:16 +0100

    The original react app content appears only on exact path "/"

--- src/components/app/mainscreen/index.tsx
+++ src/components/app/mainscreen/index.tsx
@@ -6,8 +6,9 @@ import Status from '../../status'
 import Login from '../../auth/login'
 import SignUp from '../../auth/signup'
 import MainMenu from '../../mainmenu'
-import SplashScreen from '..//splashscreen'
+import SplashScreen from '../splashscreen'
 import classnames from 'classnames'
+import Users from '../../users'
 
 export type MainScreenProps = {
   title: string
@@ -23,16 +24,18 @@ export class MainScreen extends React.Component<MainScreenProps> {
         <header className={styles.header}>
           <MainMenu title={title} subtitle={subtitle} />
         </header>
-        <div className={classnames(styles.content, "container")}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          <Status />
+        <div className={classnames(styles.content, 'container')}>
           <Switch>
+            <Route exact path="/">
+              <img src={logo} className={styles.appLogo} alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+              </a>
+              <Status />
+            </Route>
             <Route path="/login">
               <Login />
             </Route>
